29a5fb24b9f8a1a058272e6ade7ed97a03000074
Zuzia <moriendi1422@gmail.com>
2023-11-04 19:31:26 +0100

    Add files via upload

---
+++ Projects/Python/1.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+wiek = float(input('Podaj swój wiek: '))
+
+
+# In[8]:
+
+
+imie = input('Podaj swoje imie: ')
+
+
+# In[3]:
+
+
+print(wiek)
+
+
+# In[4]:
+
+
+wiek = wiek-15
+
+
+# In[5]:
+
+
+print(wiek)
+
+
+# In[13]:
+
+
+def funkcja():
+    print('Hello world')
+
+
+# In[14]:
+
+
+funkcja()
+
+
+# In[15]:
+
+
+def funkcja(imie):
+    print(f'Osoba XYZ ma na imię {imie}')
+
+
+# In[16]:
+
+
+funkcja('Zuzia')
+
+
+# In[ ]:
+
+
+
+
